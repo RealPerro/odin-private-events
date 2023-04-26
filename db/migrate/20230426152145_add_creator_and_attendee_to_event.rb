b8123dd5f58c5aa4f27f27e99afdf4785e8649e6
RealPerro <chr1@hey.com>
2023-04-26 16:34:26 +0100

    avances varios

---
+++ db/migrate/20230426152145_add_creator_and_attendee_to_event.rb
@@ -0,0 +1,6 @@
+class AddCreatorAndAttendeeToEvent < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :events, :creator, null: false, foreign_key: {to_table: :users}
+    add_reference :events, :attendee, null: true, foreign_key: {to_table: :users}
+  end
+end
