d66775fea099be6bddb9766dfd6a028b49053a85
RealPerro <chr1@hey.com>
2023-04-26 17:15:08 +0100

    no funciona

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,33 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @current_user = current_user
+    @event = Event.new
+    @event.creator_id = @current_user.id
+    @event.name = post_params[:name]
+    @event.date = post_params[:date]
+    @event.address = post_params[:address]
+    @event.description = post_params[:description]
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
+
+  private
+  def post_params
+    params.require(:event).permit(:name, :date, :address, :description)
+  end
 end
